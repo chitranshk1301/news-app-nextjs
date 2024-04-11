21ca195b26489917f6a42cb3709195e603bbba34
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2024-04-11 16:45:36 +0530

    Update page.tsx

--- app/dashboard/news/page.tsx
+++ app/dashboard/news/page.tsx
@@ -1,17 +1,19 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/navigation';
 
 const NewsDetailsPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
   const [newsDetails, setNewsDetails] = useState<any | null>(null);
 
   useEffect(() => {
     // Fetch news details based on the id
     const fetchNewsDetails = async () => {
       try {
-        const response = await fetch(`https://newsapi.org/v2/everything?id=${id}&apiKey=ca7292f2afcb4992af825c06a0db90df`);
+        const response = await fetch(
+          `https://newsapi.org/v2/everything?id=${id}&apiKey=ca7292f2afcb4992af825c06a0db90df`
+        );
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
